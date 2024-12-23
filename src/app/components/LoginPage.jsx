9924dbd4ce8abdd2952c9d53a38a5516cc81fd0c
Bruno Empke <brunoempke@Brunos-MacBook-Pro.local>
2024-12-23 20:00:28 -0300

    Started project

---
+++ src/app/components/LoginPage.jsx
@@ -0,0 +1,15 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <form>
+                <input>
+                    <label />
+                </input>
+
+                <input>
+                    <label />
+                </input>
+            </form>
+        </div>
+    )
+}
