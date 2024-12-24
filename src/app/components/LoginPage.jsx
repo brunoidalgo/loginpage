a076ce5e69df6dd1428d0afe69fa69c373b61e29
Bruno Empke <brunoempke@Brunos-MacBook-Pro.local>
2024-12-24 16:10:54 -0300

    implemented a form for user data and created a mongodb database cluster]

--- src/app/components/LoginPage.jsx
+++ src/app/components/LoginPage.jsx
@@ -1,15 +1,20 @@
 export default function LoginPage() {
     return (
-        <div>
-            <form>
-                <input>
-                    <label />
-                </input>
+        <form className="p-[400px] text-center border-solid" method="post" >
+            <h2>Login Form</h2>
+            <div className="flex flex-col justify-center items-center">
+                <label className="text-start">Username</label>
+                <input className="py-2 rounded-lg" type="username" placeholder="Enter Username" name="username" required />
 
-                <input>
-                    <label />
-                </input>
-            </form>
-        </div>
+                <label >Password</label>
+                <input className="py-2 rounded-lg" type="password" placeholder="Enter Password" name="password" required />
+            </div>
+            <div className="flex justify-center gap-4 py-4">
+                <button className="bg-white text-black p-1 rounded-lg" type="button">Submit</button>
+                <span className="bg-white text-black p-1 rounded-lg">
+                    <a href="#">Forgot password ?</a>
+                </span>
+            </div>
+        </form>
     )
 }
